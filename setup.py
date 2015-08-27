32a826346dd973374777bce5773e142f40296cc0
rbarkerappen <rbarker@appen.com>
2015-08-27 14:44:28 +1000

    Release: 2015.8.27.4.44

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+	name="appen_utilities",
+	packages=["appen_utilities"],
+	description="These are some test utils",
+	
+	version="2015.8.27.4.44",
+	
+	url="http://www.appen.com",
+	author="Appen",
+	author_email="it@appen.com",
+	
+	install_requires = [
+		"simplejson",
+	]
+)
