aaf9a04333c905595ea4977de1f96bfefadcff19
rbarkerappen <rbarker@appen.com>
2015-09-29 15:07:36 +1000

    Release: 2015.9.29.5.7

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import setup
 
 setup(
 	name="appen_utilities",
-	packages=["appen_utilities"],
+	packages=["appen_utilities.Audio"],
 	description="These are some test utils",
 	
-	version="2015.9.29.4.27",
+	version="2015.9.29.5.7",
 	
 	url="http://www.appen.com",
 	author="Appen",
